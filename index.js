d0f4e35347c88231ca008aff505334b9f09ec688
Catherine Lee <lee.catherine77@gmail.com>
2018-04-08 02:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var name = "Susan"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
+name.innerHTML = "Susan";
+height.innerHTML = "70"
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 
